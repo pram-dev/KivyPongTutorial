4cce8da7e3f6d73ec98c71bb0ccef5860bdae1af
Ramos <notpramdev@gmail.com>
2022-09-04 22:23:59 -0700

    commit

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 kivy.require("2.1.0")
 
+
 class PongGame(Widget):
     """
     PongGame inherits from kivy Widget class
     """
     pass
 
+
 class PongApp(App):
     """
     PongApp inherits from kivy App class
     """
+
     def build(self):
         return PongGame()
+
 
 if __name__ == "__main__":
     PongApp().run()
